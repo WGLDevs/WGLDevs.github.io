c7a5e22dc1e24f0c6f1cf8a7dfcd0f4968ed2934
Wellerson <wellerson.roberto@geohub.com.br>
2020-02-11 10:57:46 -0300

    Iniciando processo de migração pro Webpack

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+window.jQuery = require('jquery');
+window.$ = window.jQuery; 
+window.WOW = require('wowjs/dist/wow').WOW;
+
+require('@fancyapps/fancybox');
+require('./library/owl.carousel/owl.carousel');
+require('./library/mixitup-2.1.11/jquery.mixitup.min.js');
+require('./library/jquery-one-page-nav-3.0.0/jquery.nav.js');
+require('./scripts.js');
