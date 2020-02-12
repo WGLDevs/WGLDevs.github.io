6b0565cc33c0ad2b32b59d9eb02325a167850e8c
Wellerson <wellerson.roberto@geohub.com.br>
2020-02-12 00:46:40 -0300

    Ajustes - Retirado Isotope

--- src/js/main.js
+++ src/js/main.js
@@ -17,7 +17,6 @@ window.WOW = require('wowjs/dist/wow').WOW;
 
 require('@fancyapps/fancybox');
 require('./library/validator-0.9.0/validator.min');
-require('./library/isotoke-packaged-3.0.1/isotope.pkgd');
 require('./library/owl.carousel/owl.carousel');
 require('./library/mixitup-2.1.11/jquery.mixitup.min');
 require('./library/jquery-one-page-nav-3.0.0/jquery.nav');
