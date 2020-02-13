0097ff044ea15d777cf86f4881331cd14768da8e
Wellerson <wellerson.roberto@geohub.com.br>
2020-02-13 19:45:41 -0300

    Mudando Owl Carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,22 @@
 /* Estilo */
 require('bootstrap/dist/css/bootstrap.css');
 require('./../css/library/animate.css/animate.css');
-require('./library/owl.carousel/owl.css');
 require('@fancyapps/fancybox/dist/jquery.fancybox.css');
 require('./../css/library/fontawesome-5.12.1/css/fontawesome.min.css');
 require('./../css/library/fontawesome-5.12.1/css/solid.min.css');
 require('./../css/library/fontawesome-5.12.1/css/brands.min.css');
 require('./../css/styles.css');
 require('./../css/responsive.css');
-
+require('owl.carousel/dist/assets/owl.carousel.css');
 
 /*JavaScript*/
 window.jQuery = require('jquery');
 window.$ = window.jQuery; 
 window.WOW = require('wowjs/dist/wow').WOW;
 
+require('owl.carousel');
 require('@fancyapps/fancybox');
 require('./library/validator-0.9.0/validator.min');
-require('./library/owl.carousel/owl.carousel');
 require('./library/mixitup-2.1.11/jquery.mixitup.min');
 require('./library/jquery-one-page-nav-3.0.0/jquery.nav');
 require('./scripts');
